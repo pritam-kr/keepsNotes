b65911849e2a87a379be673219e5d8531b16750a
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2021-07-12 23:27:33 +0530

    Still working on it

--- script/app.js
+++ script/app.js
@@ -7,35 +7,50 @@ const inputContainer = document.querySelector('.input-container');
 const btnTrach = document.querySelector('.box-container');
 
 btnTrach.addEventListener('click', (e)=>{
-    console.log(e.target)
+    console.log(e.target.classList[2])
 })
-console.log(btnTrach)
+
+
 
 btnAdd.addEventListener('click', (e) =>{
     e.preventDefault()
-    console.log(inputTitle.value)
-    console.log(textArea.value)
-
-    if(inputTitle.value = ""){
-        inputTitle.value = "Untitle"
-    }
+    // console.log(inputTitle.value)
+    // console.log(textArea.value)
+    // if(inputTitle.value = ""){
+    //     inputTitle.value = "Untitle"
+    // }
+    notesList(inputTitle.value)
 
     const noteBox = document.createElement('div')
     noteBox.classList.add('notes-box')
     noteBox.innerHTML = `
-    <div class="tools">
-        <button class="btn-check"><i class="fas fa-check btn-check"></i></button>
-        <button class="btn-trash"><i class="fas fa-trash btn-trash"></i></button>
+    
+    <div class="notes-box-header">
+        <div class="notes-content">
+        <h3>${inputTitle.value}</h3>
+        </div>
+
+        <div class="tools">
+            <button class="btn-check"><i class="fas fa-check btn-check"></i></button>
+            <button class="btn-trash"><i class="fas fa-trash btn-trash"></i></button>
+        </div>
     </div>
-    <div class="notes-content">
-    <h3>${inputTitle.value}</h3>
-    <p>${textArea.value}</p>
-    </div>
+
+        <div class="text-area">
+            <p> ${textArea.value} </p>
+        </div>
 
     `
     document.querySelector('.box-container').appendChild(noteBox)
 
-    // inputTitle.value = ''
+    inputTitle.value = ''
     textArea.value = ''
 })
 
+// making list of notes
+
+function notesList(e){
+    const noteList = document.createElement('li');
+    noteList.innerHTML = e;
+    document.querySelector('.note-ul').appendChild(noteList)
+}
